46740e413f04e6f5728b9c2f5b6707f02cd67d45
yyz20002008@gmail.com <yyz20002008@gmail.com@46f74c70-3f01-f7e3-c821-069a7092331b>
2011-03-07 18:37:06 +0000

    End of Mar 7

---
+++ trunk/userManagement/insert.php
@@ -0,0 +1,18 @@
+<?php
+
+        require_once('../inc/global.php');
+              
+        $conn = getConnection();
+
+     
+      
+       
+       $conn->query("INSERT INTO People ( FirstName, LastName,Password)
+       VALUES ( $_GET[FirstName],$_GET[LastName],$_GET[Password]");
+      
+      
+      
+       $conn->close();
+       
+       require_once('index.php');
+?>
--- trunk/userManagement/userData.php
+++ trunk/userManagement/userData.php
@@ -13,4 +13,27 @@ function GetUser($id)
         $conn->close();
         return $rs;
 }
-?>
+function SaveUser($rs)
+{
+        $conn = getConnection();
+        $result = $conn->query("UPDATE People P SET FirstName='$rs[FirstName]', LastName='$rs[LastName]', Password='$rs[Password]' WHERE id=$rs[id]");
+        $error = $conn->error;
+        $conn->close();
+       
+        return $error;
+}
+function DeleteUser($id)
+{
+        $conn = getConnection();
+        $result = $conn->query("DELETE FROM People WHERE id=$id");
+        $error = $conn->error;
+        $conn->close();
+       
+        return $error;
+}
+
+function ValidateUser($rs)
+{
+        return Null;
+}
+
