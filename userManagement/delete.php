209a7922a55291dacdd9046caa9cfab93a0d6259
yyz20002008@gmail.com <yyz20002008@gmail.com@46f74c70-3f01-f7e3-c821-069a7092331b>
2011-03-10 18:37:11 +0000

    Mar 10

--- userManagement/delete.php
+++ userManagement/delete.php
@@ -1,41 +1,10 @@
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-
-	
-
 <?
         require_once('../inc/global.php');
         require_once('userData.php');
         if(isset($_REQUEST['Submit']))
         {
                 if($errors = DeleteUser($_REQUEST['id'])) print_r($errors);
-                else header("location: http://cs.newpaltz.edu/~plotkinm/WebCourse/UserManagement/");
+                else header("location: index.php");
         }else{
                 $rs = GetUser($_REQUEST['id']);
         }
@@ -49,12 +18,11 @@
 <body>
         <form method="post">
                         <div>
-                                Are you sure that you want to delete <b><?=$rs['FirstName']?> <?=$rs['LastName']?></b>
+                                Are you sure that you want to delete <b><?=$rs['FirstName']?> <?=$rs['LastName']?></b> 
                         </div>
-                       
+                        
                         <input type="submit" name="Submit" value="Submit" />
                         <a href="./">Cancel</a>
         </form>
 </body>
 </html>
-
